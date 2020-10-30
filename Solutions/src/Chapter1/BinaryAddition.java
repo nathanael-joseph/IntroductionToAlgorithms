8f87da6b905f45e5eae48fb0419936c2a4c81157
nathanael joseph <nathanaeljy@outlook.com>
2020-10-30 12:48:15 +0200

    added binary addition

---
+++ Solutions/src/Chapter1/BinaryAddition.java
@@ -0,0 +1,26 @@
+package Chapter1;
+
+public class BinaryAddition {
+    /*
+    A and B are int arrays, both of length n, containing only 0s and 1s.
+    We consider A[n-1], B[n-1], and C[n-1] to be the least important bits.
+
+    The function must return an int array C, of length n+1, s.t. C is the binary value of A + B.
+     */
+    public int[] Add(int[] A, int[] B)
+    {
+        int n = A.length;
+        int[] C = new int[n+1];
+        int i = n-1;
+        int carry = 0;
+        while (i > 0)
+        {
+            C[i+1] = (A[i] + B[i] + carry) % 2;
+            carry = (A[i] + B[i] + carry) > 1 ? 1 : 0;
+            i--;
+        }
+        C[i] = carry;
+
+        return C;
+    }
+}
--- Solutions/src/Chapter1/LinearSearch.java
+++ Solutions/src/Chapter1/LinearSearch.java
@@ -12,9 +12,15 @@ public class LinearSearch {
 
         return -1;
     }
+
+    /*
+    Loop Invariant
+    For every iteration i, there exists no k < i such that A[i] = v.
+    This is trivial for the first iteration.
+    At any given iteration, if A[i] = v, the function ends and the value of i is returned.
+    T.f. if the return statement on line 13 is reached, there exists no value i < A.length
+    s.t. A[i] = v, and so the function is correct.
+
+     */
 }
 
-/*
-Loop invariants
-
- */
