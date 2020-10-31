3abdc85e4be2e1a58917c91bfc3195fceb284d99
nathanael joseph <nathanaeljy@outlook.com>
2020-10-31 18:37:33 +0200

    added mergesort loop invariant

--- src/Chapters1to3/MergeSort.java
+++ src/Chapters1to3/MergeSort.java
@@ -35,10 +35,11 @@ public class MergeSort {
         for ( int i = 0; i < n1; i++)
             left[i] = A[p+i];
         for ( int i = 0; i < n2; i++)
-            right[i] = A[q + i];
+            right[i] = A[q + 1 + i];
 
         // li = left index, ri - right index, k main index for A.
         int li = 0; int ri =0;
+        // merge loop
         for (int k = 0; k < n1 + n2; k++)
         {
             if (li < n1 && ri < n2)
@@ -66,4 +67,23 @@ public class MergeSort {
             }
         }
     }
+
+    /*
+     Loop Invariant - Prior to each iteration of the merge lop, the sub array A[p ... p + k - 1]
+     is sorted and contains the k-1 lowest numbers from left[0 ... n1 - 1] and  right[0 ... n2 - 1]
+
+     Initialisation - before the first iteration, k = 0, and t.f. the sub array A[p ... p+k-1] is empty
+     so it is sorted trivially, and contains 0 elements from left and right arrays.
+
+     Maintenance - at iteration k, the lowest element between left[li] and right[ri] is the lowest
+     element from left[0 ... n1 - 1] and  right[0 ... n2 - 1] still not copied to A[p ... p + k - 1].
+     The element is copied and then the corresponding index is increased (either li or ri). so at the end of
+     the iteration, A[p ... p+k] contains the k-1 lowest numbers from left[0 ... n1 - 1] and
+      right[0 ... n2 - 1] in ascending order.
+
+     Termination - the merge loop terminates when k = n1 + n2 + 1, so the sub array A[p ... p + k - 1]
+     contains n1 + n2 elements from left and right arrays sorted in ascending order.
+
+     */
+
 }
--- src/Program.java
+++ src/Program.java
@@ -1,8 +1,12 @@
+import Chapters1to3.MergeSort;
+
 public class Program {
     public static void main(String[] args) {
         System.out.println("hello world");
 
-
-
+        int[] A = {4,5,8,6,5,8,2,12,46};
+        MergeSort.Sort(A);
+        for(int n : A)
+            System.out.print(n + " ");
     }
 }
