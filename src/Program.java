e57de2664ef409631d97d25bd1ebc97ec6deb006
nathanael joseph <nathanaeljy@outlook.com>
2021-01-17 20:41:09 +0200

    Started on binary search tree

--- src/Program.java
+++ src/Program.java
@@ -3,6 +3,7 @@ import Chapters6to8.MaxHeap;
 import Chapters6to8.QuickSort;
 
 public class Program {
+    
     public static void main(String[] args) {
         System.out.println("hello world");
         int[] arr = {11,4,19,13,5,7,8,1,12,23,4,14};
@@ -12,6 +13,7 @@ public class Program {
         for(int i = 0; i< sortedArr.length; i++) {
             System.out.print(sortedArr[i] + "   ");
         }
+        System.out.println("");
         sortedArr = QuickSort.QuickSort(arr);
 
         for(int i = 0; i< sortedArr.length; i++) {
